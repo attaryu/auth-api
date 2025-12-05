81e395da886b683147e3f63ccaa1e920efeec088
attaryu <mattarannaufal@gmail.com>
2025-12-05 10:26:06 +0700

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Auth API Service
+
+Layanan API sederhana untuk autentikasi pengguna menggunakan Node.js dan Hapi.js. Project ini bagian dari latihan penerapan clean architecture dalam pengembangan aplikasi backend.
+
+## Getting Started
+
+1. Clone repository ini ke lokal mesin Anda.
+2. Jalankan `pnpm install` untuk menginstal semua dependensi yang diperlukan. (Sesuaikan dengan package manager yang Anda gunakan)
+3. Copy file `.env.example` di root direktori menjadi `.env` dan isi sesuai kebutuhan.
+4. Jalankan `pnpm migrate up` untuk menjalankan migrasi database.
+5. Jalankan `pnpm start` untuk memulai server.
+
+## Upcomming Features
+
+- Mendapatkan autentikasi atau proses login
+- Memperbarui autentikasi atau refresh access token
+- Menghapus autentikasi atau proses logout
---
+++ src/Domains/users/entities/RegisterUser.js
@@ -0,0 +1,33 @@
+class RegisterUser {
+	constructor({ username, password, fullname }) {
+		this._verivyPayload({ username, password, fullname });
+
+		this.username = username;
+		this.password = password;
+		this.fullname = fullname;
+	}
+
+	_verivyPayload({ username, password, fullname }) {
+		if (!username || !password || !fullname) {
+			throw new Error('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY');
+		}
+
+		if (
+			typeof username !== 'string' ||
+			typeof password !== 'string' ||
+			typeof fullname !== 'string'
+		) {
+			throw new Error('REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
+		}
+
+		if (username.length > 50) {
+			throw new Error('REGISTER_USER.USERNAME_LIMIT_CHAR');
+		}
+
+		if (!username.match(/^[a-zA-Z1-9_]+$/)) {
+			throw new Error('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER');
+		}
+	}
+}
+
+module.exports = RegisterUser;
